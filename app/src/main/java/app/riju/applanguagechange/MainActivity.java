9657c4990c2b40f52780276b9205470a8581c612
Sudiptagupta217 <Sudiptagupta217@gmail.com>
2021-07-17 14:50:56 +0530

    set edit text and text view

--- app/src/main/java/app/riju/applanguagechange/MainActivity.java
+++ app/src/main/java/app/riju/applanguagechange/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -17,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     int lang_selected;
     RelativeLayout show_lan_dialog;
 
+    TextView firstName_TV, lastName_TV, phoneNumber_TV, email_TV, formTitle;
+    EditText firstName_ET, lastName_ET, phoneNumber_ET, email_ET;
+    Button submitBtn;
+
     Context context;
     Resources resources;
 
@@ -24,13 +30,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text=findViewById(R.id.text);
-        dialog_language=findViewById(R.id.dialog_language);
-        show_lan_dialog=findViewById(R.id.showlangdialog);
+        text = findViewById(R.id.text);
+        dialog_language = findViewById(R.id.dialog_language);
+        show_lan_dialog = findViewById(R.id.showlangdialog);
 
-        if (LocaleHelper.getLanguage(MainActivity.this).equalsIgnoreCase("en")){
-            context = LocaleHelper.setLocale(MainActivity.this,"en");
-            resources =context.getResources();
+        firstName_TV = findViewById(R.id.firstName_TV);
+        firstName_ET = findViewById(R.id.firstName_ET);
+        lastName_TV = findViewById(R.id.lastName_TV);
+        lastName_ET = findViewById(R.id.lastName_ET);
+        phoneNumber_TV = findViewById(R.id.phoneNumber_TV);
+        phoneNumber_ET = findViewById(R.id.phoneNumber_ET);
+        email_TV = findViewById(R.id.email_TV);
+        email_ET = findViewById(R.id.email_ET);
+
+        formTitle=findViewById(R.id.formTitle);
+        submitBtn = findViewById(R.id.submitBtn);
+
+        if (LocaleHelper.getLanguage(MainActivity.this).equalsIgnoreCase("en")) {
+            context = LocaleHelper.setLocale(MainActivity.this, "en");
+            resources = context.getResources();
 
             lang_selected = 0;
 
@@ -38,10 +56,12 @@ public class MainActivity extends AppCompatActivity {
 
             text.setText(resources.getString(R.string.text));
             setTitle(resources.getString(R.string.app_name));
-        }
-        else if(LocaleHelper.getLanguage(MainActivity.this).equalsIgnoreCase("hi")){
-            context = LocaleHelper.setLocale(MainActivity.this,"hi");
-            resources =context.getResources();
+
+            setString();
+
+        } else if (LocaleHelper.getLanguage(MainActivity.this).equalsIgnoreCase("hi")) {
+            context = LocaleHelper.setLocale(MainActivity.this, "hi");
+            resources = context.getResources();
 
             lang_selected = 1;
 
@@ -49,10 +69,12 @@ public class MainActivity extends AppCompatActivity {
 
             text.setText(resources.getString(R.string.text));
             setTitle(resources.getString(R.string.app_name));
-        }
-        else if(LocaleHelper.getLanguage(MainActivity.this).equalsIgnoreCase("bn")){
-            context = LocaleHelper.setLocale(MainActivity.this,"bn");
-            resources =context.getResources();
+
+            setString();
+
+        } else if (LocaleHelper.getLanguage(MainActivity.this).equalsIgnoreCase("bn")) {
+            context = LocaleHelper.setLocale(MainActivity.this, "bn");
+            resources = context.getResources();
 
             lang_selected = 2;
 
@@ -60,14 +82,17 @@ public class MainActivity extends AppCompatActivity {
 
             text.setText(resources.getString(R.string.text));
             setTitle(resources.getString(R.string.app_name));
+
+            setString();
+
         }
 
         show_lan_dialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final  String[] Language= {"ENGLISH","हिंदी","বাংলা"};
+                final String[] Language = {"ENGLISH", "हिंदी", "বাংলা"};
 
-                final AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 
                 builder.setTitle("SELECT A LANGUAGE")
                         .setSingleChoiceItems(Language, lang_selected, new DialogInterface.OnClickListener() {
@@ -75,9 +100,9 @@ public class MainActivity extends AppCompatActivity {
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 dialog_language.setText(Language[i]);
 
-                                if (Language[i].equals("ENGLISH")){
-                                    context = LocaleHelper.setLocale(MainActivity.this,"en");
-                                    resources =context.getResources();
+                                if (Language[i].equals("ENGLISH")) {
+                                    context = LocaleHelper.setLocale(MainActivity.this, "en");
+                                    resources = context.getResources();
 
                                     lang_selected = 0;
 
@@ -85,10 +110,12 @@ public class MainActivity extends AppCompatActivity {
 
                                     text.setText(resources.getString(R.string.text));
                                     setTitle(resources.getString(R.string.app_name));
+
+                                    setString();
                                 }
-                                if (Language[i].equals("हिंदी")){
-                                    context = LocaleHelper.setLocale(MainActivity.this,"hi");
-                                    resources =context.getResources();
+                                if (Language[i].equals("हिंदी")) {
+                                    context = LocaleHelper.setLocale(MainActivity.this, "hi");
+                                    resources = context.getResources();
 
                                     lang_selected = 1;
 
@@ -96,10 +123,12 @@ public class MainActivity extends AppCompatActivity {
 
                                     text.setText(resources.getString(R.string.text));
                                     setTitle(resources.getString(R.string.app_name));
+
+                                    setString();
                                 }
-                                if (Language[i].equals("বাংলা")){
-                                    context = LocaleHelper.setLocale(MainActivity.this,"bn");
-                                    resources =context.getResources();
+                                if (Language[i].equals("বাংলা")) {
+                                    context = LocaleHelper.setLocale(MainActivity.this, "bn");
+                                    resources = context.getResources();
 
                                     lang_selected = 2;
 
@@ -107,6 +136,7 @@ public class MainActivity extends AppCompatActivity {
 
                                     text.setText(resources.getString(R.string.text));
                                     setTitle(resources.getString(R.string.app_name));
+                                    setString();
                                 }
                             }
                         }).setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -118,5 +148,19 @@ public class MainActivity extends AppCompatActivity {
                 builder.create().show();
             }
         });
+    }
+    void setString(){
+        firstName_TV.setText(resources.getString(R.string.first_name));
+        lastName_TV.setText(resources.getString(R.string.last_name));
+        email_TV.setText(resources.getString(R.string.email));
+        phoneNumber_TV.setText(resources.getString(R.string.phone_number));
+
+        firstName_ET.setHint(resources.getString(R.string.enter_your_first_name));
+        lastName_ET.setHint(resources.getString(R.string.enter_your_last_name));
+        email_ET.setHint(resources.getString(R.string.enter_your_email_id));
+        phoneNumber_ET.setHint(resources.getString(R.string.enter_your_phone_number));
+
+        formTitle.setText(resources.getString(R.string.fill_form));
+        submitBtn.setText(resources.getString(R.string.submit));
     }
 }
